1620512dd6a0d69ade719d3de157e17582b3bc33
OhCGGK <OhCGGK@babb.cc>
2026-09-23 22:58:12 +0800

    fix: update repository references in release template and changelog tool
    - Changed repository links in the release template to use a generic placeholder for better adaptability.
    - Updated the repository constant in the changelog tool to reflect the new repository name.
    - Removed unused Telegram bot service from the build workflow to streamline the process.

--- tool/changelog.dart
+++ tool/changelog.dart
@@ -7,7 +7,7 @@ import 'src/changelog/git.dart';
 import 'src/changelog/models.dart';
 import 'src/changelog/render.dart';
 
-const _repository = 'chen08209/FlClash';
+const _repository = 'ohmycggk/FlClash';
 
 Future<void> main(List<String> arguments) async {
   final parser = ArgParser()
